5e0c4874936b232d6de734ad3e9f58c3986922a8
Demnik2000 <demnik2282000@gmail.com>
2024-10-14 22:26:27 +0300

    Done. M3:task1-2

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,4 +1,7 @@
+# from logging import Manager
+
 from django.db import models
+from django.db.models import Manager
 
 # Create your models here.
 
@@ -7,3 +10,4 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveBigIntegerField()
+    objects: Manager
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,9 +1,6 @@
-from lib2to3.fixes.fix_input import context
-
-from django.db.models.expressions import result
+# from MainApp.models import Item
 from django.http import HttpResponseNotFound, HttpResponse
 from django.shortcuts import render
-from django.template.context_processors import request
 
 author = {
         'Имя': 'Иван',
@@ -13,21 +10,16 @@ author = {
         "email": "vasya@mail.ru"
     }
 
-items = [
-    {"id": 1, "name": "Кроссовки abibas", "quantity":5},
-    {"id": 2, "name": "Кожанная куртка", "quantity":2},
-    {"id": 5, "name": "Coca-cola 1 литр", "quantity":12},
-    {"id": 7, "name": "Картофель фри", "quantity":0},
-    {"id": 8, "name": "Кепка", "quantity":124},
-]
+# items = [
+#     {"id": 1, "name": "Кроссовки abibas", "quantity":5},
+#     {"id": 2, "name": "Кожанная куртка", "quantity":2},
+#     {"id": 5, "name": "Coca-cola 1 литр", "quantity":12},
+#     {"id": 7, "name": "Картофель фри", "quantity":0},
+#     {"id": 8, "name": "Кепка", "quantity":124},
+# ]
 
 
 def home(request):
-    # text = """
-    # <h1>"Извучаем django"</h1>
-    # <strong>Автор</strong>: <i>Иванов И.П.</i>
-    # """
-    # return HttpResponse(text)
     context = {
         "name": "Иванов Петр Семенович",
         "email": "my_mail@mail.ru"
@@ -49,15 +41,18 @@ def about(request):
 
 def get_item(request, item_id: int):
     """ По указанному id возвращает элемент из списка """
-    for item in items:
-        if item['id'] == item_id:
-            context = {"item": item}
-            return  render(request, "item_page.html", context)
-    return HttpResponseNotFound(f'Item with id={item_id} not found')
+    try:
+        item = Item.objects.get(id=item_id)
+    except Item.DoesNotExist:
+        return HttpResponseNotFound(f'Item with id={item_id} not found')
+    else:
+        context = {"item": item}
+        return  render(request, "item_page.html", context)
+
 
 
 def get_items(request):
-
+    items = Item.objects.all()
     context = {"items": items}
     return render(request, "items_list.html", context)
 
