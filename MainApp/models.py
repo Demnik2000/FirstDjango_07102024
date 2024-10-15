5abc8b52cf48fee2d396d51e3fbe3fc2f3ed2cf3
Demnik2000 <demnik2282000@gmail.com>
2024-10-15 16:09:59 +0300

    Add Color

--- MainApp/models.py
+++ MainApp/models.py
@@ -6,12 +6,14 @@ from pygments.lexer import default
 
 
 # Create your models here.
-
+class Color(models.Model):
+    name = models.CharField(max_length=32, unique=True)
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveBigIntegerField()
     description = models.TextField(max_length=200, default="Базовое описание")
+    colors = models.ManyToManyField(to=Color)
 
     objects: Manager
